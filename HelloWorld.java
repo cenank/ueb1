8edc8f3cdfe8a922f829dc0d8d47aa371872b484
hundehausen <mail@grischadaum.de>
2020-11-04 19:00:07 +0100

    added print command from hundehausen

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("5 Jahre an der Uni");
+	System.out.println("Die Aenderung des Partners");
     }
 }
